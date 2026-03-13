3fa0079ec5403b58e0403f90558662075c2b6d59
d3F0g <joqeewee@gmail.com>
2026-03-13 17:57:38 +0800

    Inventory and orders done

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import { Hono } from 'hono'
 import inventory from './routes/inventory'
+import order from './routes/order'
 
 const app = new Hono()
 
 app.route("/inventory", inventory)
+app.route("/order", order)
 
 export default app
--- src/routes/inventory.ts
+++ src/routes/inventory.ts
@@ -3,17 +3,26 @@ import { db } from "../firebaseAdmin";
 
 const inventory = new Hono()
 
+const isExpired = (value: unknown) => {
+  if (typeof value !== "number" || !Number.isFinite(value)) {
+    return false
+  }
+
+  return value * 1000 < Date.now()
+}
+
 // Add one food
 inventory.post('/', async (c) => {
   try {
     const body = await c.req.json()
-    const {name, quantity, supplier, expiry} = body
+    const {name, quantity, supplier, expiry, price} = body
 
     const docRef = await db.collection('inventory').add({
       name,
       quantity,
       supplier,
       expiry,
+      price,
       createdAt: new Date().toISOString(),
     })
 
@@ -26,16 +35,20 @@ inventory.post('/', async (c) => {
 
 
 // Get ALL documents in the inventory collection
+// Get all inventory that is not expired
 inventory.get('/', async (c) => {
   const snapshot = await db.collection('inventory').get()
-  const data = snapshot.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data()
-  }))
+  const data = snapshot.docs
+    .filter((doc) => !isExpired(doc.data().expiry))
+    .map((doc) => ({
+      id: doc.id,
+      ...doc.data()
+    }))
+
   return c.json(data)
 })
 
-// Get ONE document
+// Get ONE document (dont care if expired or not)
 inventory.get('/:id', async (c) => {
   const id = c.req.param('id')
   const doc = await db.collection('inventory').doc(id).get()
