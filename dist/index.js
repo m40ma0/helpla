1659c6594c20d4628e678de5a1eb5b67bdaef80b
d3F0g <joqeewee@gmail.com>
2026-03-13 18:38:06 +0800

    Updated firebase config

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-import { serve } from '@hono/node-server';
-import app from './app.js';
-const PORT = 3000;
-serve({
-    fetch: app.fetch,
-    port: PORT
-});
-console.log(`Server running on port ${PORT}`);
--- src/firebaseAdmin.ts
+++ src/firebaseAdmin.ts
@@ -3,11 +3,20 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+
+const privateKey = process.env.FIREBASE_PRIVATE_KEY
+if (!privateKey) {
+  throw new Error('Missing FIREBASE_PRIVATE_KEY')
+}
+
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
-    projectId: process.env.FIREBASE_PROJECT_ID
-  });
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: privateKey.replace(/\\n/g, '\n'),
+    }),
+  })
 }
 
 export const db = admin.firestore();
