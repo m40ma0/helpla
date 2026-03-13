cc60d716e1390d241ecf5cd358278d692cc82fa7
d3F0g <joqeewee@gmail.com>
2026-03-13 18:23:35 +0800

    Customer routes
    - Add a customer
    - Retrieve all customers
    - Retrieve one customer

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import { Hono } from 'hono'
 import inventory from './routes/inventory'
 import order from './routes/order'
+import customer from './routes/customer'
 
 const app = new Hono()
 
 app.route("/inventory", inventory)
 app.route("/order", order)
+app.route("/customer", customer)
 
 export default app
