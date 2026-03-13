ea3b696d6afd9ccccffe4db9366460e0a130404d
d3F0g <joqeewee@gmail.com>
2026-03-13 19:01:30 +0800

    Removed dist

--- dist/app.js
+++
@@ -1,9 +0,0 @@
-import { Hono } from 'hono';
-import inventory from './routes/inventory.js';
-import order from './routes/order.js';
-import customer from './routes/customer.js';
-const app = new Hono();
-app.route("/inventory", inventory);
-app.route("/order", order);
-app.route("/customer", customer);
-export default app;
--- dist/index.js
+++
@@ -1,7 +0,0 @@
-import { serve } from '@hono/node-server';
-import app from './app.js';
-serve({
-    fetch: app.fetch,
-    port: 3000
-});
-console.log('Running on http://localhost:3000');
